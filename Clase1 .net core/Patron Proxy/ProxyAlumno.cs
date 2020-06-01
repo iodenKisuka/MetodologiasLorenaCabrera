502c2d14f1b250b249b1c8c3bcfe95201b91c03c
iodenKisuka <iodenteken@hotmail.com>
2020-06-01 16:09:53 -0300

    Hice el ejercicio 1 que me pedia hacer un proxy para Alumno y Alumno Estudioso

--- Clase1 .net core/Patron Proxy/ProxyAlumno.cs
+++ Clase1 .net core/Patron Proxy/ProxyAlumno.cs
@@ -5,23 +5,42 @@ using System.Text;
 
 namespace Clase1_.net_core.Patron_Proxy
 {
-    class ProxyAlumno: Alumno
+    class ProxyAlumno : InterfaceAlumnoparaProxy
     {
         Alumno AlumnoReal;
 
-        public  Alumno RespondioLAPregunta(int pregunta,int nota)
-        {
-            if (nota ==pregunta % 3)
-            {
-               return (AlumnoMuyEstudioso)FabricaCreadorIComprable.CrearAleatorio(FabricaCreadorIComprable.CrearAlumnoEstudioso) ;
-            }
-            else
-            {
-                return (Alumno)FabricaCreadorIComprable.CrearAleatorio(FabricaCreadorIComprable.CrearAlumno);
-            }
 
-            //return pregunta % 3;
+        public int ResponderPregunta(int pregunta)
+        {
+            if (AlumnoReal == null)
+            {
+                int NumAlum;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Que clase de estudiante quiere llamar :" + "\n Peresione 1_Alumno o 2- Alumno Estudioso");
+                        NumAlum = int.Parse(Console.ReadLine());
+                        if (NumAlum == 2)
+                        {
+                            AlumnoReal = (AlumnoMuyEstudioso)FabricaCreadorIComprable.CrearAleatorio(FabricaCreadorIComprable.CrearAlumnoEstudioso);
+
+                        }
+                        else if (NumAlum == 1)
+                        {
+                            AlumnoReal = (Alumno)FabricaCreadorIComprable.CrearAleatorio(FabricaCreadorIComprable.CrearAlumno);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        NumAlum = 0;
+                    }
+                } while (NumAlum < 1 && NumAlum > 2);
+  
+            }
+            return AlumnoReal.responderPregunta(pregunta);
         }
+
 
     }
 }
